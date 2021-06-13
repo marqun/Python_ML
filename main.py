10bd0628451f0ee077d169d02cd73374e81ed6e8
marqun <35145079+marqun@users.noreply.github.com>
2021-06-13 09:26:49 +0200

    tfimport

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import tensorflow as tf  # now import the tensorflow module
+print(tf.version)  # make sure the version is 2.x
 
-x = [1, 2, 2.5, 3, 4]
-y = [1, 4, 7, 9, 15]
-plt.plot(x, y, 'ro')
-plt.axis([0, 6, 0, 20])
+tensor1 = tf.zeros([1, 2, 3])  # tf.ones() creates a shape [1,2,3] tensor full of ones
+tensor2 = tf.reshape(tensor1, [6])  # reshape existing data to shape [2,3,1]
+tensor3 = tf.reshape(tensor2, [3, -1])  # -1 tells the tensor to calculate the size of the dimension in that place
+# this will reshape the tensor to [3,3]
+
+# The numer of elements in the reshaped tensor MUST match the number in the original
+
+print(tensor1)
+print(tensor2)
+print(tensor3)
